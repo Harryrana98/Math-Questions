9ed41ec66633d3189f3710c5dcb08592407446fd
Harryrana98 <ranaharendra77@gmail.com>
2025-04-03 11:36:32 +0530

    update

--- script.js
+++ script.js
@@ -1,67 +1,69 @@
+const Questions = [
+  {
+    q: "2+2-1",
+    a: "3",
+    options: ["4", "5", "3", "6"],
+  },
+  {
+    q: "2+8-1",
+    a: "9",
+    options: ["7", "8", "9", "6"],
+  },
+  {
+    q: "2*5-10",
+    a: "0",
+    options: ["10", "5", "0", "20"],
+  },
+  {
+    q: "2(5+3)-5",
+    a: "11",
+    options: ["4", "10", "11", "6"],
+  },
+  {
+    q: "4+3+6-7-1",
+    a: "5",
+    options: ["4", "5", "8", "6"],
+  },
+];
 
-const Questions=[
-    {
-        q:"2+2-1",
-        a:"3",
-        options:["4","5","3","6"]
-    },
-    {
-        q:"2+8-1",
-        a:"9",
-        options:["7","8","9","6"]
-    },
-    {
-        q:"2*5-10",
-        a:"0",
-        options:["10","5","0","20"]
-    },
-    {
-        q:"2(5+3)-5",
-        a:"11",
-        options:["4","10","11","6"]
-    },
-    {
-        q:"4+3+6-7-1",
-        a:"5",
-        options:["4","5","8","6"]
-    }
-]
+const startBtn = document.querySelector("#start");
+const quizContainer = document.querySelector(".gameContainer");
+const quesDiv = document.querySelector("#ques");
+const optDiv = document.querySelector("#opt");
+const timerDiv = document.querySelector("#timer");
 
-    
-const startBtn=document.querySelector("#start")
-const quizContainer=document.querySelector(".gameContainer")
-const quesDiv=document.querySelector("#ques")
-const optDiv=document.querySelector("#opt")
-const timerDiv=document.querySelector("#timer") 
+const timer=0
 
-startBtn.addEventListener("click",startQuiz)
+startBtn.addEventListener("click", startQuiz);
 
-function startQuiz(){
-    startBtn.style.display="none"
-    
-    quesDiv.innerHTML=Questions[RandomQuiz()].q
-    // opt.innerHTML=Questions[RandomQuiz()].options
-    OptionDisplay() 
-    
-    console.log(quizContainer);
+function startQuiz() {
+  startBtn.style.display = "none";
 
+  quesDiv.innerHTML = Questions[RandomQuiz()].q;
+  // opt.innerHTML=Questions[RandomQuiz()].options
+  const randomOptions = Questions[RandomQuiz()].options;
+
+  OptionDisplay(randomOptions);
+
+  timerDiv.innerHTML=timer
+  const counter = setInterval(() => {
+    if()
+  
+      timerDiv.innerHTML++;
+  }, 1000);
 }
 
-function OptionDisplay(optArr){
-    optArr.forEach((option)=>{
-        const optBtn=document.createElement("button")
-        optBtn.className="optBtn"
-        optBtn.innerHTML=option
-        optDiv.append(optBtn)
-        console.log(optBtn);
-        
-    })
+function OptionDisplay(optArr) {
+  optArr.forEach((option) => {
+    const optBtn = document.createElement("button");
+    optBtn.className = "optBtn";
+    optBtn.innerHTML = option;
+    optDiv.append(optBtn);
+
+  });
 }
 
-
-
-function RandomQuiz(){
-    const random=Math.floor(Math.random()*Questions.length)
-    return random;
+function RandomQuiz() {
+  const random = Math.floor(Math.random() * Questions.length);
+  return random;
 }
-
